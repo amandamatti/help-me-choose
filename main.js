1351caac5e9576926e95bb681cf130fee3d2ae27
Amanda Matti <ammis_matti@hotmail.com>
2018-10-04 10:59:00 +0200

    Added function to go-button

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 /*   Tjejer Kodar
      Help me choose
-     index.html
+     main.js
      Amanda Matti, 2018 */
 
 // Göm resultSection när sidan laddas
@@ -16,12 +16,32 @@ $(function() {
 
   //Vid klick -> kör add_option
   $('.addOption').click(add_option)
+
+  var options = [];
+  var submit = function() {
+    $('.inputSection').hide()
+    $('.resultSection').css('display', 'flex')
+    $('input').each(function() {
+      options.push($(this).val())
+    });
+    var winner = options[Math.floor(Math.random() * options.length)]
+    console.log(winner);
+    $('.resultSection').text(winner)
+  }
+  $('.go').click(submit);
 });
 
-//Vid klick på rubriken byts bakgrundsfärgen
+//Vid klick på rubriken byts färgen
 //Klura mer på denna senare
-/*$(function(){
-    $('.clickhere').click(function() {
-        $('.bgSection').css('background-color', 'yellow');
-    });
-}); */
+/*
+$(".clickhere").click(function() {
+  $(this).toggleClass("color");
+});
+*/
+
+/*BONUS
+När resultatet visas, byt ut rubriken från "Help me choose between.."
+till "The winner is:"
+Använd en if-sats för att se till att en input-text bara läggs till
+options-listan om det finns någon text i det, dvs om värdet inte
+är "" (tom sträng)
